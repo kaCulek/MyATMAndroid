41c69c534e4fc0ec92e3dfa3bbface87d093adf3
kaCulek <karlo2316@gmail.com>
2021-05-16 18:13:09 +0200

    Add project files.

---
+++ MyATMAndroid/DashboardActivity.cs
@@ -0,0 +1,52 @@
+﻿
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Java.Interop;
+
+namespace MyATMAndroid
+{
+    [Activity(Label = "DashboardActivity")]
+    public class DashboardActivity : Activity
+    {
+        private string userCard;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.dashboard);
+            userCard = Intent.GetStringExtra("UserCard") ?? string.Empty;
+        }
+
+        [Export("WithdrawalClick")]
+        public void WithdrawalClick(Android.Views.View e)
+        {
+            var activity = new Intent(this, typeof(WithdrawalActivity));
+            activity.PutExtra("UserCard", userCard);
+            StartActivity(activity);
+        }
+
+        [Export("ShowCurrentBalance")]
+        public void ShowCurrentBalance(Android.Views.View e)
+        {
+            var activity = new Intent(this, typeof(CurrentBalanceActivity));
+            activity.PutExtra("UserCard", userCard);
+            StartActivity(activity);
+        }
+
+        [Export("LastTransactionsClick")]
+        public void LastTransactionsClick(Android.Views.View e)
+        {
+            var activity = new Intent(this, typeof(LastTransactionsActivity));
+            activity.PutExtra("UserCard", userCard);
+            StartActivity(activity);
+        }
+
+        [Export("LogoutClick")]
+        public void LogoutClick(Android.Views.View e)
+        {
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
